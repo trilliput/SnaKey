8d1998dbec69ed792c9c3f28e8f68afbcc7fecb0
Ilya Shmygol <ishmygol@gmail.com>
2020-10-17 16:22:13 +0200

    Fix colors for MacOS

--- game.py
+++ game.py
@@ -903,7 +903,7 @@ class SnaKeyGUI(tk.Tk):
         if self.pause_button['text'] == 'pause':
             self.pause_button.configure(
                 text='un-pause',
-                bg='SystemButtonHighlight', )
+                bg='SystemHighlightText')
             # Disable player movement:
             self.unbind('<Key>')
             self.after_cancel(self.chaser_cancel_id)
@@ -914,7 +914,7 @@ class SnaKeyGUI(tk.Tk):
         else:
             self.pause_button.configure(
                 text='pause',
-                bg='SystemButtonFace', )
+                bg='SystemButtonText', )
             # Unfreeze player and enemy movement:
             self.bind('<Key>', self.move_player)
             self.chaser_cancel_id = self.after(800, self.move_chaser)
@@ -983,7 +983,7 @@ class SnaKeyGUI(tk.Tk):
         self.pause_button['state'] = 'disabled'
 
         # Highlight the restart button:
-        self.restart_button['bg'] = 'SystemButtonHighlight'
+        self.restart_button['bg'] = 'SystemHighlightText'
 
     def update_cs(self, cs: str = None):
         """
@@ -997,7 +997,7 @@ class SnaKeyGUI(tk.Tk):
             self.cs = cs
 
         # Recolor the menu:
-        self.restart_button.configure(bg='SystemButtonFace')
+        self.restart_button.configure(bg='SystemButtonText')
 
         # Recolor all tiles:
         self.grid.configure(cs['lines'])
