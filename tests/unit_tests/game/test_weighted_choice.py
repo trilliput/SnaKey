50f9245ca34e721290d8bc6a98557ef23e240a0f
Ilya Shmygol <ishmygol@gmail.com>
2020-12-09 20:11:36 +0100

    Cover game.weighted_choice

--- tests/unit_tests/game/test_weighted_choice.py
+++ tests/unit_tests/game/test_weighted_choice.py
@@ -13,3 +13,32 @@ def test_weighted_choice_with_empty_weights():
 
     # Assert
     assert expected_excetpion.match('This should not happen.')
+
+
+def test_weighted_choice_with_non_integer_values():
+    """
+    If argument weights contains non integer values
+    then weighted_choice raises an exception
+    """
+    # Act & Assert
+    with pytest.raises(TypeError):
+        weighted_choice({'key': 'string'})
+
+
+def test_weighted_choice():
+    """
+    In a list of 100 results of weighted_choice they key with much greater value appears more often
+    """
+    # Arrange
+    input = {'lower': 0, 'a': 1, 'b': 2, 'c': 3, 'greater': 10}
+    counts = {key: 0 for key in input}
+
+    # Act
+    for _ in range(100):
+        actual_result = weighted_choice(input)
+        counts[actual_result] += 1
+
+    # Assert
+    assert 'lower' in counts
+    assert 'greater' in counts
+    assert counts['lower'] < counts['greater']
